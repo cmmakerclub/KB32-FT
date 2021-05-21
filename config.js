efd15e12c864db72706d5b494c581f54619241e4
woodif <wooddy.202@gmail.com>
2021-05-21 17:48:09 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "kb32-ft",
+  vendor: "MakerAsia",
+  platform: "arduino-esp32",
+  title: "KB32-FT (Arduino API)",
+  description: "KB32-FT Compatible Kidbright32",
+  author: "Maker Asia",
+  website: "http://www.kidbright.club",
+  email: "info@makerasia.com",
+  git: "https://github.com/woodif/kb32-ft/",
+  image: "/static/display.png",
+  version: "1.1.0",
+};
