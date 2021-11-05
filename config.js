26d1287688b9f5fa6f2803122fe1f2bf5c6e7bcb
Wasin Wongkum <kkmonster@hotmail.com>
2021-11-05 20:24:51 +0700

    update version

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ module.exports = {
   email: "info@makerasia.com",
   git: "https://github.com/cmmakerclub/kb32-ft/",
   image: "/static/display.png",
-  version: "1.2.2",
+  version: "1.2.3",
   recommended: "ilovekbide",
 };
